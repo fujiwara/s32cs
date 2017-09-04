efd47e63439df70652f553ca2f02689830917bd1
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2017-09-04 11:10:07 +0900

    move codes from functions to s32cs package.

---
+++ apex.go
@@ -0,0 +1,43 @@
+package s32cs
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"regexp"
+
+	apex "github.com/apex/go-apex"
+	"github.com/aws/aws-sdk-go/aws/session"
+)
+
+func ApexRun() {
+	var reg *regexp.Regexp
+	if r := os.Getenv("KEY_REGEXP"); r != "" {
+		reg = regexp.MustCompile(r)
+	} else {
+		reg = nil
+	}
+	client := NewClient(session.New(), os.Getenv("ENDPOINT"), reg)
+
+	handler := func(msg json.RawMessage, ctx *apex.Context) (interface{}, error) {
+		var event SQSEvent
+		if err := json.Unmarshal(msg, &event); err != nil {
+			return nil, err
+		}
+		if event.QueueURL != "" {
+			log.Printf("starting process sqs %s", event.QueueURL)
+			return nil, client.ProcessSQS(event.QueueURL)
+		}
+		var s3event S3Event
+		if err := json.Unmarshal(msg, &s3event); err != nil {
+			return nil, err
+		}
+		log.Printf("starting process s3 %s", s3event)
+		if err := client.Process(s3event); err != nil {
+			return nil, err
+		}
+		return true, nil
+	}
+
+	apex.HandleFunc(handler)
+}
--- functions/upload/main.go
+++ functions/upload/main.go
@@ -1,45 +1,9 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
-	"os"
-	"regexp"
-
-	apex "github.com/apex/go-apex"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/fujiwara/s32cs"
 )
 
-var client *s32cs.Client
-
-func run(msg json.RawMessage, ctx *apex.Context) (interface{}, error) {
-	var event s32cs.SQSEvent
-	if err := json.Unmarshal(msg, &event); err != nil {
-		return nil, err
-	}
-	if event.QueueURL != "" {
-		log.Printf("starting process sqs %s", event.QueueURL)
-		return nil, client.ProcessSQS(event.QueueURL)
-	}
-	var s3event s32cs.S3Event
-	if err := json.Unmarshal(msg, &s3event); err != nil {
-		return nil, err
-	}
-	log.Printf("starting process s3 %s", s3event)
-	if err := client.Process(s3event); err != nil {
-		return nil, err
-	}
-	return true, nil
-}
-
 func main() {
-	var reg *regexp.Regexp
-	if r := os.Getenv("KEY_REGEXP"); r != "" {
-		reg = regexp.MustCompile(r)
-	} else {
-		reg = nil
-	}
-	client = s32cs.NewClient(session.New(), os.Getenv("ENDPOINT"), reg)
-	apex.HandleFunc(run)
+	s32cs.ApexRun()
 }
